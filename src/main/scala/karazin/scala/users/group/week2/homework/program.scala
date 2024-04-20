3675e0a7545bfaf55faf82d2dcbb39cf6918972d
Artem_Ziablov <113183341+proge6@users.noreply.github.com>
2024-04-20 21:25:30 +0200

    week2 - homework

--- src/main/scala/karazin/scala/users/group/week2/homework/program.scala
+++ src/main/scala/karazin/scala/users/group/week2/homework/program.scala
@@ -14,22 +14,54 @@ object program:
    Do not change the result type, it must be `ErrorOr[List[PostView]]`
   */
   def getPostsViews(apiKey: String)(commentsFilter: List[Comment] => Boolean,
-                                    likesFilter: List[Like] => Boolean): ErrorOr[List[PostView]] = ???
+                                    likesFilter: List[Like] => Boolean): ErrorOr[List[ErrorOr[PostView]]] =
+    for
+      profile <- getUserProfile(apiKey)
+      posts <- getPosts(profile.userId)
+      postsView <- ErrorOr(posts map { post => getPostView(post)(commentsFilter, likesFilter) })
+    yield postsView
 
   // Desugared
   def getPostsViewDesugared(apiKey: String)(commentsFilter: List[Comment] => Boolean,
-                                            likesFilter: List[Like] => Boolean): ErrorOr[List[PostView]] = ???
+                                            likesFilter: List[Like] => Boolean): ErrorOr[List[ErrorOr[PostView]]] =
+    getUserProfile(apiKey) flatMap { profile =>
+      getPosts(profile.userId)
+    } map { posts =>
+      posts map { post =>
+        getComments(post.postId).withFilter(commentsFilter) flatMap { comments =>
+          getLikes(post.postId).withFilter(likesFilter) flatMap { likes =>
+            getShares(post.postId) map { shares =>
+              PostView(post, comments, likes, shares)
+            }
+          }
+        }
+      }
+    }
 
   /* 
     Getting view for a particular user's post
     Provide an argument and a result type
   */
   def getPostView(post: Post)(commentsFilter: List[Comment] => Boolean,
-                              likesFilter: List[Like] => Boolean): ErrorOr[PostView] = ???
+                              likesFilter: List[Like] => Boolean): ErrorOr[PostView] =
+    for
+      comments  <- getComments(post.postId)
+      if commentsFilter(comments)
+      likes     <- getLikes(post.postId)
+      if likesFilter(likes)
+      shares    <- getShares(post.postId)
+    yield PostView(post, comments, likes, shares)
 
   // Desugared
   def getPostViewDesugared(post: Post)(commentsFilter: List[Comment] => Boolean,
-                                       likesFilter: List[Like] => Boolean): ErrorOr[PostView] = ???
+                                       likesFilter: List[Like] => Boolean): ErrorOr[PostView] =
+    getComments(post.postId).withFilter(commentsFilter) flatMap { comments =>
+      getLikes(post.postId).withFilter(likesFilter) flatMap { likes =>
+        getShares(post.postId) map { shares =>
+          PostView(post, comments, likes, shares)
+        }
+      }
+    }
 
 
 
