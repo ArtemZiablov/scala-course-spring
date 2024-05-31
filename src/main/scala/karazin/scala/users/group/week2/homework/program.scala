912fc2197d283fa2adaa782bf359adba8fb84b27
Artem Ziablov <150075410+ArtemZiablov@users.noreply.github.com>
2024-05-31 11:27:58 +0200

    Week2 (#2)
    * week2 - homework
    
    * fixed ??? according to my naming
    
    * fixed one test
    
    * change some comments and getPostsViews
    
    * add UserId & PostId
    
    * implement getPostsViewDesugared
    
    * add ApiKey and change services
    
    * fix buggs in program
    
    ---------
    
    Co-authored-by: Artem_Ziablov <113183341+proge6@users.noreply.github.com>

--- src/main/scala/karazin/scala/users/group/week2/homework/program.scala
+++ src/main/scala/karazin/scala/users/group/week2/homework/program.scala
@@ -1,35 +1,77 @@
 package karazin.scala.users.group.week2.homework
 
 // Do not forget to import custom implementation
+
 import karazin.scala.users.group.week2.homework.adt.*
 import karazin.scala.users.group.week2.homework.model.*
+import karazin.scala.users.group.week2.homework.model.ApiKey.ApiKey
 import karazin.scala.users.group.week2.homework.services.*
 
 object program:
-  
+
   /*
    Getting view for all user's posts
    Provide a result type
   
    Do not change the result type, it must be `ErrorOr[List[PostView]]`
   */
-  def getPostsViews(apiKey: String)(commentsFilter: List[Comment] => Boolean,
-                                    likesFilter: List[Like] => Boolean): ErrorOr[List[PostView]] = ???
+  def getPostsViews(apiKey: ApiKey)(commentsFilter: List[Comment] => Boolean,
+                                    likesFilter: List[Like] => Boolean): ErrorOr[List[PostView]] =
+    for
+      profile <- getUserProfile(apiKey)
+      posts <- getPosts(profile.userId)
+      postsView <- posts.foldLeft(ErrorOr(List.empty[PostView])) {
+        (acc, elem) =>
+          acc flatMap
+            (list => getPostView(elem)(commentsFilter, likesFilter) map (list :+ _))
+      }
+    yield postsView
 
   // Desugared
-  def getPostsViewDesugared(apiKey: String)(commentsFilter: List[Comment] => Boolean,
-                                            likesFilter: List[Like] => Boolean): ErrorOr[List[PostView]] = ???
-
-  /* 
+  def getPostsViewDesugared(apiKey: ApiKey)(commentsFilter: List[Comment] => Boolean,
+                                            likesFilter: List[Like] => Boolean): ErrorOr[List[PostView]] =
+    getUserProfile(apiKey) flatMap { profile =>
+      getPosts(profile.userId) flatMap { posts =>
+        posts.foldLeft(ErrorOr(List.empty[PostView])) {
+          (acc, elem) =>
+            acc flatMap { list =>
+              getComments(elem.postId).withFilter(commentsFilter) flatMap { comments =>
+                getLikes(elem.postId).withFilter(likesFilter) flatMap { likes =>
+                  getShares(elem.postId) map { shares =>
+                    PostView(elem, comments, likes, shares)
+                  }
+                }
+              } map (list :+ _)
+            }
+        }
+      }
+    }
+  
+  /*
     Getting view for a particular user's post
     Provide an argument and a result type
   */
   def getPostView(post: Post)(commentsFilter: List[Comment] => Boolean,
-                              likesFilter: List[Like] => Boolean): ErrorOr[PostView] = ???
+                              likesFilter: List[Like] => Boolean): ErrorOr[PostView] =
+    for
+      comments <- getComments(post.postId)
+      if commentsFilter(comments)
+      likes <- getLikes(post.postId)
+      if likesFilter(likes)
+      shares <- getShares(post.postId)
+    yield PostView(post, comments, likes, shares)
 
   // Desugared
   def getPostViewDesugared(post: Post)(commentsFilter: List[Comment] => Boolean,
-                                       likesFilter: List[Like] => Boolean): ErrorOr[PostView] = ???
+                                       likesFilter: List[Like] => Boolean): ErrorOr[PostView] =
+    getComments(post.postId).withFilter(commentsFilter) flatMap { comments =>
+      getLikes(post.postId).withFilter(likesFilter) flatMap { likes =>
+        getShares(post.postId) map { shares =>
+          PostView(post, comments, likes, shares)
+        }
+      }
+    }
+
 
 
 
